653bcf2eb41d7da60e6c2b8920dfc4e8b3718347
Alexander Wilson van Deurs <alexwvd@outlook.com>
2018-07-26 15:24:40 +0200

    init

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(publicPath))
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`)
+})

9d345a30260c595e19c5c52ca6a568e82d783319
Alexander Wilson van Deurs <alexwvd@outlook.com>
2018-08-06 17:32:15 +0200

    added autoscrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 let socket = io();
 
+function scrollToBottom () {
+    const messages = jQuery('#messages');
+    const newMessage = messages.children('li:last-child');
+
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function (){
     console.log('Connected to server!');
 });
@@ -14,6 +29,7 @@ socket.on('newMessage', function (message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -25,6 +41,7 @@ socket.on('newLocationMessage', function (message) {
         url: message.url
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('disconnect', function (){
